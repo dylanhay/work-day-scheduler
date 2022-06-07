792e06f2f5947aafeda70d36a6714912ceb5e6ea
dchay <dylanhay@rogers.com>
2022-06-06 23:36:50 -0400

    t1 const defined

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
 let dateDisplay = document.getElementById("currentDay");
 
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let today = new Date();
+var dd = String(today.getDate()).padStart(2, "0");
+var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
+today = mm + "/" + dd + "/" + yyyy;
 
 dateDisplay.innerHTML = today;
 
+const t1 = new Date().getHours();
+
+console.log(t1);
 
 
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-console.log(time);
