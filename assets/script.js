f7ab6435b41a87c2e886f3a5e36ef942edc1ad32
dchay <dylanhay@rogers.com>
2022-06-08 17:38:19 -0400

    editable content introduced

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ let dateDisplay = document.getElementById("currentDay");
 
 let today = new Date();
 var dd = String(today.getDate()).padStart(2, "0");
-var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+var mm = String(today.getMonth() + 1).padStart(2, "0"); 
 var yyyy = today.getFullYear();
 
 today = mm + "/" + dd + "/" + yyyy;
