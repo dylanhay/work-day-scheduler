f6b5510286812d10cf56cd96d11c060202dc86dc
dchay <dylanhay@rogers.com>
2022-06-08 18:44:47 -0400

    save btn deployed for ind element

--- assets/script.js
+++ assets/script.js
@@ -70,6 +70,40 @@ function twelveHrConverter () {
 
 
 
+var text = document.getElementById('cmon');
+var myData;
+var postData = window.localStorage.getItem("save");
+// var reset = text.innerHTML;
+// if no data
+if (postData == null || postData == '') {
+    myData = text.innerHTML;
+    // store default value
+    window.localStorage.setItem("save", myData);
+    // make it placeholder style
+    text.classList.remove('changed');
+} else {
+    // if there is a value post it
+    text.innerHTML = postData;
+    // make dark text
+    text.classList.add('changed');
+}
+
+function saveChanges() {
+    // store the current value
+    myData = text.innerHTML;
+    // local store the value
+    window.localStorage.setItem("save", myData);
+    text.classList.add('changed');
+}
 
 
 
+
+// let saveBtn = document.querySelector('.saveBtn');
+
+// saveBtn.addEventListener('click', () => {
+
+//   if(content.contentEditable === 'false') {
+//   	localStorage.setItem('content', content.innerHTML);
+//   }
+// });
